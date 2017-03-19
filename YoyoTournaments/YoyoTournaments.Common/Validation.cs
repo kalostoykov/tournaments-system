0da49ab0e7dcc2bcec46bc6fb57a3fb47470f3b3
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2017-03-19 16:55:49 +0200

    change division validation constants and populate the db with the division types

--- YoyoTournaments/YoyoTournaments.Common/Validation.cs
+++ YoyoTournaments/YoyoTournaments.Common/Validation.cs
@@ -5,11 +5,11 @@
         public const int MinNameLength = 3;
         public const int MaxNameLength = 50;
 
-        public const int MinDivisionNameLength = 3;
+        public const int MinDivisionNameLength = 1;
         public const int MaxDivisionNameLength = 20;
 
         public const int MinDivisionDescriptionLength = 3;
-        public const int MaxDivisionDescriptionLength = 100;
+        public const int MaxDivisionDescriptionLength = 500;
 
         public const int MinCountryNameLength = 3;
         public const int MaxCountryNameLength = 120;
