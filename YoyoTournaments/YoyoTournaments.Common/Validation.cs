889b750f9136c6d1c1274ea4e6849b1451a228d8
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2017-03-19 16:21:09 +0200

    add common validation constants

---
+++ YoyoTournaments/YoyoTournaments.Common/Validation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoyoTournaments.Common
+{
+    public static class Validation
+    {
+        public const int MinNameLength = 3;
+        public const int MaxNameLength = 50;
+
+        public const int MinDivisionNameLength = 3;
+        public const int MaxDivisionNameLength = 20;
+
+        public const int MinCountryNameLength = 3;
+        public const int MaxCountryNameLength = 120;
+
+        public const int MinTournamentTitleLength = 6;
+        public const int MaxTournamentTitleLength = 20;
+    }
+}
