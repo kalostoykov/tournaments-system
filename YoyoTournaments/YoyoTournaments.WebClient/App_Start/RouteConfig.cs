bab311284043f38106b3c3c5f503751a0fec5902
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2017-03-21 19:35:11 +0200

    all urls to lowercase

--- YoyoTournaments/YoyoTournaments.WebClient/App_Start/RouteConfig.cs
+++ YoyoTournaments/YoyoTournaments.WebClient/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace YoyoTournaments.WebClient
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.LowercaseUrls = true;
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
