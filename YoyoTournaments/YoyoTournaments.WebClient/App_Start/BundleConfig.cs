85d1e7cf65852974d0b8c290eea6cc53b2ccb199
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2017-03-25 19:47:48 +0200

    change the site theme

--- YoyoTournaments/YoyoTournaments.WebClient/App_Start/BundleConfig.cs
+++ YoyoTournaments/YoyoTournaments.WebClient/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace YoyoTournaments.WebClient
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootswatch-darkly-bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
