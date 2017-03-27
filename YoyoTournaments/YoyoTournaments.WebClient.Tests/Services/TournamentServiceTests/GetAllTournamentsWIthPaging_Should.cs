3798449f6cd35e3e57291d114eeaa51fa35d6dae
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2017-03-27 09:28:10 +0300

    more tests

--- YoyoTournaments/YoyoTournaments.WebClient.Tests/Services/TournamentServiceTests/GetAllTournamentsWIthPaging_Should.cs
+++ YoyoTournaments/YoyoTournaments.WebClient.Tests/Services/TournamentServiceTests/GetAllTournamentsWIthPaging_Should.cs
@@ -65,48 +65,152 @@ namespace YoyoTournaments.WebClient.Tests.Services.TournamentServiceTests
         //
         #endregion
 
-        //[TestMethod]
-        //public void ReturnCountEqualsToOne_WhenTournamentsCountainsOneTournament()
-        //{
-        //    // Arrange
-        //    int expectedCount = 1;
-        //    int page = 1;
-        //    int pageSize = 1;
+        [TestMethod]
+        public void ReturnCountEqualsToOne_WhenTournamentsCountainsOneTournament()
+        {
+            // Arrange
+            int page = 1;
+            int pageSize = 1;
 
-        //    Guid id = Guid.NewGuid();
-        //    Guid countryId = Guid.NewGuid();
+            Guid id = Guid.NewGuid();
+            Guid countryId = Guid.NewGuid();
 
-        //    Tournament expectedTournament = new Tournament
-        //    {
-        //        Id = id,
-        //        Name = "Test tournament",
-        //        StartDate = DateTime.Now,
-        //        EndDate = DateTime.Now,
-        //        Place = "Test place",
-        //        CountryId = countryId
-        //    };
+            Tournament expectedTournament = new Tournament
+            {
+                Id = id,
+                Name = "Test tournament",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now,
+                Place = "Test place",
+                CountryId = countryId
+            };
 
-        //    var expectedTournaments = new List<Tournament>()
-        //    {
-        //        expectedTournament
-        //    };
+            var expectedTournaments = new List<Tournament>()
+            {
+                expectedTournament
+            };
 
-        //    var tournamentDbSetMock = QueryableDbSetMock.GetQueryableMockDbSet(expectedTournaments);
-        //    tournamentDbSetMock.Setup(x => x.ToList()).Returns(expectedTournaments);
+            var tournamentDbSetMock = QueryableDbSetMock.GetQueryableMockDbSet(expectedTournaments);
 
-        //    var yoyoTournamentsDbContextMock = new Mock<IYoyoTournamentsDbContext>();
-        //    yoyoTournamentsDbContextMock.Setup(x => x.Tournaments.ToList()).Returns(tournamentDbSetMock.Object.ToList());
+            var yoyoTournamentsDbContextMock = new Mock<IYoyoTournamentsDbContext>();
+            yoyoTournamentsDbContextMock.Setup(x => x.Tournaments).Returns(tournamentDbSetMock.Object);
 
-        //    var divisionTypeServiceMock = new Mock<IDivisionTypeService>();
+            var divisionTypeServiceMock = new Mock<IDivisionTypeService>();
 
-        //    var tournamentService = new TournamentService(yoyoTournamentsDbContextMock.Object, divisionTypeServiceMock.Object);
+            var tournamentService = new TournamentService(yoyoTournamentsDbContextMock.Object, divisionTypeServiceMock.Object);
 
-        //    // Act
-        //    var actualCount = 0;
-        //    var actualTournaments = tournamentService.GetAllTournamentsWIthPaging(out actualCount, page, pageSize);
+            // Act
+            var actualCount = 0;
+            var actualTournaments = tournamentService.GetAllTournamentsWIthPaging(out actualCount, page, pageSize);
 
-        //    // Assert
-        //    Assert.AreEqual(expectedCount, actualCount);
-        //}
+            // Assert
+            Assert.AreEqual(expectedTournaments.Count, actualCount);
+        }
+
+        [TestMethod]
+        public void ReturnCollection_WithPageSizeNumberOfItems()
+        {
+            // Arrange
+            int page = 1;
+            int pageSize = 2;
+
+            Guid id = Guid.NewGuid();
+            Guid countryId = Guid.NewGuid();
+
+            Tournament expectedTournament = new Tournament
+            {
+                Id = id,
+                Name = "expected tournament",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now,
+                Place = "expected place",
+                CountryId = countryId
+            };
+
+            var expectedTournaments = new List<Tournament>()
+            {
+                new Tournament
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Test",
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now,
+                    Place = "place",
+                    CountryId = countryId
+                },
+                expectedTournament
+            };
+
+            var tournamentDbSetMock = QueryableDbSetMock.GetQueryableMockDbSet(expectedTournaments);
+
+            var yoyoTournamentsDbContextMock = new Mock<IYoyoTournamentsDbContext>();
+            yoyoTournamentsDbContextMock.Setup(x => x.Tournaments).Returns(tournamentDbSetMock.Object);
+
+            var divisionTypeServiceMock = new Mock<IDivisionTypeService>();
+
+            var tournamentService = new TournamentService(yoyoTournamentsDbContextMock.Object, divisionTypeServiceMock.Object);
+
+            // Act
+            var actualCount = 0;
+            var actualTournaments = tournamentService.GetAllTournamentsWIthPaging(out actualCount, page, pageSize);
+
+            // Assert
+            Assert.AreEqual(pageSize, actualTournaments.Count());
+        }
+
+        [TestMethod]
+        public void ReturnCollection_ForTheProvidedPage_OrderedByStartDate()
+        {
+            // Arrange
+            int page = 2;
+            int pageSize = 1;
+
+            Guid id = Guid.NewGuid();
+            Guid countryId = Guid.NewGuid();
+
+            Tournament expectedTournament = new Tournament
+            {
+                Id = id,
+                Name = "expected tournament",
+                StartDate = DateTime.Now.Add(new TimeSpan(1, 0, 0, 0)),
+                EndDate = DateTime.Now.Add(new TimeSpan(2, 0, 0, 0)),
+                Place = "expected place",
+                CountryId = countryId
+            };
+
+            var expectedTournaments = new List<Tournament>()
+            {
+                expectedTournament,
+                new Tournament
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Test",
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now,
+                    Place = "place",
+                    CountryId = countryId
+                }
+            };
+
+            var tournamentDbSetMock = QueryableDbSetMock.GetQueryableMockDbSet(expectedTournaments);
+
+            var yoyoTournamentsDbContextMock = new Mock<IYoyoTournamentsDbContext>();
+            yoyoTournamentsDbContextMock.Setup(x => x.Tournaments).Returns(tournamentDbSetMock.Object);
+
+            var divisionTypeServiceMock = new Mock<IDivisionTypeService>();
+
+            var tournamentService = new TournamentService(yoyoTournamentsDbContextMock.Object, divisionTypeServiceMock.Object);
+
+            // Act
+            var actualCount = 0;
+            var actualTournaments = tournamentService.GetAllTournamentsWIthPaging(out actualCount, page, pageSize);
+
+            // Assert
+            Assert.AreEqual(
+                expectedTournaments
+                .OrderBy(x => x.StartDate)
+                .ToList()[page - 1].Name,
+                actualTournaments.First().Name);
+        }
     }
 }
